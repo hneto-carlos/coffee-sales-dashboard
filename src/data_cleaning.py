60a42021c9132c1dca33b6eb0934fc2108991ed5
Carlos Hass Neto <carloshassneto@Carloss-MacBook-Air.local>
2026-04-16 11:53:17 +0800

    Small clean comments

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,21 +1,12 @@
 import pandas as pd
 
-
-# =========================
 # 1. LOAD DATA
-# =========================
 df = pd.read_excel('data/raw/coffee_shop_sales.xlsx')
 
-
-# =========================
 # 2. STANDARDIZE COLUMN NAMES
-# =========================
 df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
 
-
-# =========================
 # 3. DATA TYPE CORRECTIONS
-# =========================
 # Convert date
 df['transaction_date'] = pd.to_datetime(df['transaction_date'])
 
@@ -24,17 +15,12 @@ df['transaction_time'] = pd.to_datetime(
     df['transaction_time'], format='%H:%M:%S'
 )
 
-
-# =========================
 # 4. CREATE BUSINESS METRICS
-# =========================
 # Revenue
 df['revenue'] = df['transaction_qty'] * df['unit_price']
 
-
-# =========================
 # 5. CREATE TIME FEATURES
-# =========================
+
 df['hour'] = df['transaction_time'].dt.hour
 df['day'] = df['transaction_date'].dt.day_name()
 df['month'] = df['transaction_date'].dt.month_name()
@@ -43,10 +29,8 @@ df['month'] = df['transaction_date'].dt.month_name()
 df['day_num'] = df['transaction_date'].dt.dayofweek
 df['month_num'] = df['transaction_date'].dt.month
 
-
-# =========================
 # 6. SELECT FINAL COLUMNS
-# =========================
+
 df_export = df[[
     'transaction_date',
     'hour',
@@ -62,10 +46,7 @@ df_export = df[[
     'revenue'
 ]]
 
-
-# =========================
-# 7. FINAL CLEANING (OPTIONAL BUT SAFE)
-# =========================
+# 7. FINAL CLEANING
 # Remove missing values if any
 df_export = df_export.dropna()
 
@@ -74,11 +55,7 @@ df_export['transaction_qty'] = df_export['transaction_qty'].astype(int)
 df_export['unit_price'] = df_export['unit_price'].astype(float)
 df_export['revenue'] = df_export['revenue'].astype(float)
 
-
-# =========================
 # 8. EXPORT CLEAN DATA
-# =========================
 df_export.to_csv('data/processed/coffee_sales_clean.csv', index=False)
 
-
-print("✅ Data cleaned and exported successfully.")
+print("Data cleaned and exported successfully.")
